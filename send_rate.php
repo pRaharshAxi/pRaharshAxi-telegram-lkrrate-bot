6838ea0b7c563f33f98eb22dbcddf1d904ad6563
pRaharshAxi <as2023514@sci.sjp.ac.lk>
2026-03-25 17:30:49 +0530

    Added GitHub Actions automation

--- send_rate.php
+++ send_rate.php
@@ -1,25 +1,18 @@
 <?php
 
-$bot_token = "8699504125:AAF8EfkSdjWfdaJpa67uUuk97TDL6tf117Y";
-$chat_id = "2141804755";
+$bot_token = getenv('BOT_TOKEN');
+$chat_id  = getenv('CHAT_ID');
 
-// Get exchange rate
 $response = file_get_contents("https://api.exchangerate-api.com/v4/latest/USD");
 $data = json_decode($response, true);
 
 $lkr_rate = $data['rates']['LKR'];
 
-// Message
-$message = "💱 USD to LKR Rate Today:\n1 USD = " . $lkr_rate . " LKR";
+$message = "💱 USD to LKR Today:\n1 USD = $lkr_rate LKR";
 
-// Send to Telegram
 $url = "https://api.telegram.org/bot$bot_token/sendMessage";
 
-$params = [
+file_get_contents($url . '?' . http_build_query([
     'chat_id' => $chat_id,
     'text' => $message
-];
-
-file_get_contents($url . '?' . http_build_query($params));
-
-echo "Message sent!";
+]));
